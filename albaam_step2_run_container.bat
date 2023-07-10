e7a8805da27aeeaef394427a4934ce21803d0c7c
candykim-dev <candykim@albaam.com>
2023-07-11 01:27:44 +0900

    오타 수정

--- albaam_step2_run_container.bat
+++ albaam_step2_run_container.bat
@@ -6,4 +6,4 @@ docker run --cpus=16 --memory=32g ^
     -it mediapipe_unity:linux
 
 
-REM python build.py build --bazel_build_opts "--experimental_scale_timeous=10.0 --http_timeout_scaling=10.0" --android arm64 --android_ndk_api_level 21 --linkopt=-s -vv
+REM python build.py build --bazel_build_opts "--experimental_scale_timeouts=10.0 --http_timeout_scaling=10.0" --android arm64 --android_ndk_api_level 21 --linkopt=-s -vv
