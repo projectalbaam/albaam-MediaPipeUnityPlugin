76dfbc9c8a252599612df9bf07ce5de3ac595745
candykim-dev <candykim@albaam.com>
2023-07-11 01:11:00 +0900

    16g -> 32g

--- albaam_step2_run_container.bat
+++ albaam_step2_run_container.bat
@@ -1,9 +1,9 @@
 Rem Run with `Packages` directory mounted to the container
 Rem Specify `--cpus` and `--memory` options according to your machine.
-docker run --cpus=16 --memory=16g ^
+docker run --cpus=16 --memory=32g ^
     --mount type=bind,src=%CD%\Packages,dst=/home/mediapipe/Packages ^
     --mount type=bind,src=%CD%\Assets,dst=/home/mediapipe/Assets ^
     -it mediapipe_unity:linux
 
 
-REM python build.py build --bazel_build_opts "--experimental_scale_timeouts=10.0 --http_timeout_scaling=10.0" --android arm64 --android_ndk_api_level 21 --linkopt=-s -vv
+REM python build.py build --bazel_build_opts "--experimental_scale_timeous=10.0 --http_timeout_scaling=10.0" --android arm64 --android_ndk_api_level 21 --linkopt=-s -vv
